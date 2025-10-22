c3a911c23d6b5fe796ec50780b7a471f4ca8949d
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2025-10-22 08:38:38 -0300

    AT-32 fix: tratar logs

--- src/mqtt_client.py
+++ src/mqtt_client.py
@@ -5,6 +5,15 @@ import time
 import paho.mqtt.client as mqtt
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)  
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)  
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
 
 message_buffer = [] 
 buffer_lock = threading.Lock() 
