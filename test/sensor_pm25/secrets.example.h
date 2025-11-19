efa3575a57168f5fb223fa33748986ac3db5ab94
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2025-11-18 21:27:07 -0300

    AT-72 adicionar conexão com broker MQTT

---
+++ test/sensor_pm25/secrets.example.h
@@ -0,0 +1,16 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// Configurações de Rede (Wi-Fi)
+const char* SECRET_SSID = "SEU_WIFI"; 
+const char* SECRET_PWD = "SENHA_DO_WIFI";   
+
+// Configurações do Servidor MQTT
+const char* SECRET_MQTT_SERVER = "BROKER_SERVER";
+const int SECRET_MQTT_PORT = 'PORTA DO BROKER '; // [INT] PORTA DO BROKER 
+const char* SRECET_MQTT_TOPIC = "BROKER_TOPIC";
+
+// Configurações do servidor NTP
+const char* SECRET_NTP_SERVER = "SERVER_NTP";
+
+#endif // SECRETS_H
